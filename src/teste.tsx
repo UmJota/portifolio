fbcf41e51aac7a06d16e96b783ef5d661d94a927
Jota <jpedrost90@gmail.com>
2024-09-13 19:52:38 -0300

    first commit

---
+++ src/teste.tsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao fontSize="18px" principal>
+        Enviar
+      </Botao>
+      <Botao fontSize="14px" principal={false}>
+        Clique aqui
+      </Botao>
+      <BotaoPerigo principal>Não Clique aqui</BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
